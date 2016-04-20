ad1eb72a3b5b28ec6db5af86a67429eb1ebd13b2
chalmie <blairchalmers@gmail.com>
2016-04-20 15:04:51 -0700

    display JSON response in  RestaurantActivity list item layout

--- app/src/main/java/com/epicodus/myrestaurant/ui/RestaurantActivity.java
+++ app/src/main/java/com/epicodus/myrestaurant/ui/RestaurantActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epicodus.myrestaurant.R;
+import com.epicodus.myrestaurant.adapters.RestaurantListAdapter;
 import com.epicodus.myrestaurant.models.Restaurant;
 import com.epicodus.myrestaurant.services.YelpService;
 
@@ -27,10 +30,10 @@ import okhttp3.Response;
 
 public class RestaurantActivity extends AppCompatActivity {
     public static final String TAG = RestaurantActivity.class.getSimpleName();
-    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
-    @Bind(R.id.locationTextView) TextView mLocationTextView;
-    @Bind(R.id.listView) ListView mListView;
     private Toast mToastToShow;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,36 +41,10 @@ public class RestaurantActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurant);
         ButterKnife.bind(this);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                int toastDurationInMilliseconds = 500;
-                String restaurant = ((TextView)view).getText().toString();
-                mToastToShow = Toast.makeText(RestaurantActivity.this, restaurant, Toast.LENGTH_LONG);
-
-                CountDownTimer toastCountDown;
-                toastCountDown = new CountDownTimer(toastDurationInMilliseconds, 1000) {
-                    @Override
-                    public void onTick(long l) {
-                        mToastToShow.show();
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        mToastToShow.cancel();
-                    }
-                };
-
-                mToastToShow.show();
-                toastCountDown.start();
-            }
-        });
-
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         getRestaurants(location);
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 
     private void getRestaurants(String location) {
@@ -86,22 +63,12 @@ public class RestaurantActivity extends AppCompatActivity {
                 RestaurantActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        String[] restaurantNames = new String[mRestaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++) {
-                            restaurantNames[i] = mRestaurants.get(i).getName();
-                        }
-                        ArrayAdapter adapter = new ArrayAdapter(RestaurantActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
-                        for (Restaurant restaurant : mRestaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getName());
-                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-                        }
                     }
                 });
             }
